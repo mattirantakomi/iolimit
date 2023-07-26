9c58fa0d5488b0d865435828d7654c86e63ae3d0
Matti Rantakömi <matti@rantakomi.fi>
2023-07-26 23:12:47 +0300

    return unique disks

--- layers/entrypoint.sh
+++ layers/entrypoint.sh
@@ -2,7 +2,7 @@
 
 CGROUPPATH=$(find /sys/fs/cgroup/kubepods -name "*$POD_UID*")
 
-for DEVICE in $(lsblk -l | grep -v NAME | egrep "disk|part" | awk '{ print $2 }'); do
+for DEVICE in $(lsblk -l | grep -v NAME | egrep "disk|part" | awk '{ print $2 }' | uniq); do
     echo $DEVICE rbps=$RBPS riops=$RIOPS wbps=$WBPS wiops=$WIOPS
     echo $DEVICE rbps=$RBPS riops=$RIOPS wbps=$WBPS wiops=$WIOPS > $CGROUPPATH/io.max
 done
