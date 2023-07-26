a01880b2b441692ea0599125f17b57bfdcf9e2d8
Matti Rantakömi <matti@rantakomi.fi>
2023-07-26 23:29:48 +0300

    log cgrouppath to init container stdout

--- layers/entrypoint.sh
+++ layers/entrypoint.sh
@@ -3,8 +3,8 @@
 CGROUPPATH=$(find /sys/fs/cgroup/kubepods -name "*$POD_UID*")
 
 for DEVICE in $(lsblk -l | grep -v NAME | grep " disk " | awk '{ print $2 }' | uniq); do
-    echo $DEVICE rbps=$RBPS riops=$RIOPS wbps=$WBPS wiops=$WIOPS
     echo $DEVICE rbps=$RBPS riops=$RIOPS wbps=$WBPS wiops=$WIOPS > $CGROUPPATH/io.max
 done
 
+echo "Configured limits for CGROUPPATH $CGROUPPATH:"
 cat $CGROUPPATH/io.max
